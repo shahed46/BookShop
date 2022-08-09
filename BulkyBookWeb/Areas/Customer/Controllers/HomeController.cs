494b48fad424fb2c80f2f99d9d40e69c99c0d478
shahed46 <50131097+shahed46@users.noreply.github.com>
2022-08-09 11:59:35 +0600

    implemented area

--- BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 
 namespace BulkyBookWeb.Controllers
 {
+    [Area("Customer")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
